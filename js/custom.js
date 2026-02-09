fb81095cdb1da7f361ebfc2f2d76378d4bbfe71a
Muhamad Arwin Wijaya <54936971+marwinwijaya@users.noreply.github.com>
2026-02-09 11:52:34 +0700

    Auto Add Sertifikat

--- js/custom.js
+++ js/custom.js
@@ -297,29 +297,33 @@
 
 
 	var portfolioMasonry = function() {
- $('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-          filter: data
-        })
-      });
+		var $grid = null;
 
+		if (document.getElementById("section-certificate")) {
+			$grid = $(".filters-content .grid").isotope({
+				itemSelector: ".all",
+				percentPosition: true,
+				layoutMode: "fitRows",
+				getSortData: {
+					year: ".year parseInt"
+				},
+				sortBy: "year",
+				sortAscending: false
+			});
+		}
 
-      if(document.getElementById("section-portfolio")){
-            var $grid = $(".grid").isotope({
-              itemSelector: ".all",
-              percentPosition: true,
-              masonry: {
-                columnWidth: ".all"
-              }
-            })
-      };
-
-
-        };
+		$('.filters ul li').click(function(){
+			$('.filters ul li').removeClass('active');
+			$(this).addClass('active');
+			
+			var data = $(this).attr('data-filter');
+			if ($grid) {
+				$grid.isotope({
+					filter: data
+				});
+			}
+		});
+	};
 
         var backToTop = function() {
             var btn = $('.back-to-top');
