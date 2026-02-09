bdb0ede8663d8a9d93ba376ac65dd9428f272390
Muhamad Arwin Wijaya <54936971+marwinwijaya@users.noreply.github.com>
2026-02-09 14:37:16 +0700

    GTAG

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,9 +1,18 @@
 (function () {
+  var gaId = "G-CVZKHH1X52";
+
+  if (!document.querySelector('script[src^="https://www.googletagmanager.com/gtag/js?id="]')) {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=" + gaId;
+    document.head.appendChild(script);
+  }
+
   window.dataLayer = window.dataLayer || [];
   function gtag() {
     window.dataLayer.push(arguments);
   }
   window.gtag = window.gtag || gtag;
   gtag("js", new Date());
-  gtag("config", "G-CVZKHH1X52");
+  gtag("config", gaId);
 })();
